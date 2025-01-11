804435e1c6ea3afc5e4d6285123b8ef98cf93287
daniel.soares <danielsoares28@hotmail.com>
2025-01-11 13:42:19 -0300

    Ajustando a pasta de saída do Switch

--- builds/build_com_docker.ps1
+++ builds/build_com_docker.ps1
@@ -11,9 +11,13 @@ function Main {
         docker run --rm -v $volume -e DEVKITPRO=/opt/devkitpro zelda3 sh -c 'cd platform/switch && make'
         docker run --rm -v $volume zelda3 make RUNTIME=$runtime LANGUAGE=$lang TARGET_EXEC=''
 
-        New-Item -ItemType Directory -Force -Path "switch"
-        Copy-Item "../platform/switch/zelda3.ini" "switch/zelda3.ini" 
-        Move-Item "../platform/switch/zelda3.nro" "switch/zelda3.nro"
+        New-Item -ItemType Directory -Force -Path "switch/zelda3"
+        Copy-Item "../platform/switch/zelda3.ini" "switch/zelda3/zelda3.ini" -Force
+        Move-Item "../platform/switch/zelda3.nro" "switch/zelda3/zelda3.nro" -Force
+        Move-Item "switch/zelda3_assets.dat" "switch/zelda3/zelda3_assets.dat" -Force
+        Remove-Item "switch/zelda3.ini" -Force
+
+        echo "Copie a pasta zelda3 para dentro da pasta `"/switch`" do Cartão SD do Switch" > "switch/README.txt"
     } else {
         docker run --rm -v $volume zelda3 make RUNTIME=$runtime LANGUAGE=$lang
     }
